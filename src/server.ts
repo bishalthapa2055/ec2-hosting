f13cff8bf3bfd5f8d570e156307f86b855a4624a
bishalthapa2055 <bishalthapa2055@gmail.com>
2024-08-04 08:06:03 +0545

    refactor

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import { app } from './app';
 const start = async() =>{
     console.log("Starting up........");
     const server = require("http").createServer(app);
-      server.listen(process.env.port, () => {
+      server.listen(process.env.port || 3000 , () => {
         console.log(
           `server sttarted at port:http://localhost:${process.env.port}`
         );
