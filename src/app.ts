59d472d217bf73ab57c996cacdd051eded5ff160
bishalthapa2055 <bishalthapa2055@gmail.com>
2024-08-06 08:06:12 +0545

    refactgor

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(json());
 
 
 app.get("/", (req , res) =>{
-    res.status(200).json({status : true , message : "Server is Live from express"})
+    res.status(200).json({status : true , message : "Server Live from express"})
 })
 
 
